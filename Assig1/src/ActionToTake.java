dbd8865848d9303d01d5a46fa4bf45152a720a45
Ming Shen <mshen2@outlook.com>
2017-01-24 21:13:26 -0500

    action class

---
+++ Assig1/src/ActionToTake.java
@@ -0,0 +1,12 @@
+
+public class ActionToTake {
+	String orient_move;
+	String straight_move;
+	int action_cost;
+	
+	public ActionToTake() {
+		this.orient_move = null;
+		this.straight_move = null;
+		this.action_cost = -1;
+	}
+}
