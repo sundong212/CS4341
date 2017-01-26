b60f69f8ffc6aafaccb0f29e4e05e9916e215a3f
Ming Shen <mshen2@wpi.edu>
2017-01-25 19:56:44 -0500

    queueType

--- Assig1/src/QueueType.java
+++ Assig1/src/QueueType.java
@@ -1,4 +1,10 @@
 
 public class QueueType {
-
+	Terrain terrain_inside;
+	int h_plus_G;
+	
+	public QueueType(Terrain terrain_inside, int h_plus_G) {
+		this.terrain_inside = terrain_inside;
+		this.h_plus_G = h_plus_G;
+	}
 }
